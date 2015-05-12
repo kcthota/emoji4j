9a80b58a3c12a2f7d605638a5c57082f8cb62ace
kc <kcthota@gmail.com>
2015-05-11 23:52:46 -0700

    added emoticon tests

---
+++ src/test/java/emoji4j/EmoticonTest.java
@@ -0,0 +1,29 @@
+package emoji4j;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class EmoticonTest {
+	@Test
+	public void testEmojiByEmoticons() {
+		Emoji emoji = EmojiUtils.getEmoji(":)");
+		assertThat(emoji.getEmoji()).isEqualTo("😃");
+
+		emoji = EmojiUtils.getEmoji(";]");
+		assertThat(emoji.getEmoji()).isEqualTo("😉");
+
+		// invalid emoticon
+		emoji = EmojiUtils.getEmoji(";|D");
+		assertThat(emoji).isNull();
+	}
+
+	@Test
+	public void testParseEmoticons() {
+		String text=":):-),:-):-]:-xP=*:*<3:P:p,=-)";
+		assertThat(EmojiUtils.emojify(text)).isEqualTo("😃😃😅😃😶😝😗😗❤️😛😛😅");
+		
+		text = "A :cat:, :dog: and a :mouse: became friends<3. For :dog:'s birthday party, they all had :hamburger:s, :fries:s, :cookie:s and :cake:.";
+		assertThat(EmojiUtils.emojify(text)).isEqualTo("A 🐱, 🐶 and a 🐭 became friends❤️. For 🐶's birthday party, they all had 🍔s, 🍟s, 🍪s and 🍰.");
+	}
+}
